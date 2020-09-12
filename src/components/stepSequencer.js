96c71029c8f6d3b67701a471defa4f17c7c33d48
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-09-12 12:22:05 -0400

    Get basic multi-track workflow setup

--- src/components/stepSequencer.js
+++ src/components/stepSequencer.js
@@ -7,13 +7,15 @@ const StepSequencer = () => {
   const numBeats = 8;
   const loop = useRef(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [beatState, setBeatState] = useState(Array(numBeats).fill(false));
+  const [beatState, setBeatState] = useState([Array(numBeats).fill(false)]);
+
+  const soundFiles = {
+    A1: "A1.mp3",
+    A2: "A2.mp3",
+  };
   
   const keys = new Players({
-      urls: {
-        A1: "A1.mp3",
-        A2: "A2.mp3",
-      }, 
+      urls: soundFiles, 
       baseUrl: "https://tonejs.github.io/audio/casio/",
       fadeOut: "64n",
       onload: () => {
@@ -23,48 +25,64 @@ const StepSequencer = () => {
   ).toDestination();
 
   useEffect(() => {
+    console.log(beatState);
     if (loop.current) {
       loop.current.dispose();
     }
 
-    loop.current = new Sequence((time, col) => {
-      if (beatState[col]) {
-        console.log(col, time);
-        keys.player('A1').start(Time(), 0, "16t", 0);
-      }
-    }, [...new Array(numBeats)].map((_, i) => i), '8n').start(0);
-  }, [beatState]);
+    const soundFileKeys = Object.keys(soundFiles);
 
-  const handePlay = () => {
+    loop.current = new Sequence((time, col) => {
+      beatState.map((track, row) => {
+        if (track[col]) {
+          console.log(`Col: ${col}; Row: ${row}; Time: ${time}`);
+          keys.player(soundFileKeys[row % soundFileKeys.length]).start(Time(), 0, "16t", 0);
+        }
+      });
+    }, [...new Array(numBeats)].map((_, i) => i), '8n').start(0);
+  }, [keys, beatState]);
+
+  const play = () => {
     Transport.start();
   }
 
-  const handleStop = () => {
+  const stop = () => {
     Transport.stop();
   }
 
-  const handleNoteClick = (e) => {
+  const noteClick = (e) => {
     const noteState = e.target.checked;
-    const {col} = e.target.dataset;
+    const {col, row} = e.target.dataset;
     const newBeatState = cloneDeep(beatState);
-    newBeatState[col] = noteState;
+    newBeatState[row][col] = noteState;
     setBeatState(newBeatState)
+  }
+
+  const addTrack = () => {
+    const newBeatState = cloneDeep(beatState);
+    setBeatState([...newBeatState, Array(numBeats).fill(false)])
   }
 
   return (
     <div>
-      <button disabled={!isLoaded} onClick={handePlay}>Play</button>
-      <button disabled={!isLoaded} onClick={handleStop}>Stop</button>
+      <button disabled={!isLoaded} onClick={play}>Play</button>
+      <button disabled={!isLoaded} onClick={stop}>Stop</button>
+      <button disabled={!isLoaded} onClick={addTrack}>Add Track</button>
       <StepSequencer.BeatGrid>
         {
-          beatState.map((isChecked, idx) => (
-            <input 
-              type="checkbox" 
-              onChange={handleNoteClick} 
-              value={isChecked} 
-              data-col={idx} 
-              key={idx} 
-            />
+          beatState.map((track, trackIdx) => (
+            <StepSequencer.Track key={trackIdx}>
+              {track.map((isChecked, idx) => (
+                <input 
+                  type="checkbox" 
+                  onChange={noteClick} 
+                  value={isChecked} 
+                  data-col={idx}
+                  data-row={trackIdx}
+                  key={idx} 
+                />
+              ))}
+            </StepSequencer.Track>
           ))
         }
       </StepSequencer.BeatGrid>
@@ -75,6 +93,13 @@ const StepSequencer = () => {
 StepSequencer.BeatGrid = styled.div`
   display: flex;
   margin: 2rem;
+  flex-direction: column;
 `;
+
+StepSequencer.Track = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+`;
+
 
 export default StepSequencer;
