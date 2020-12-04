b3146cf3ad97d3041bb138adc4c208ff18f0d841
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-12-04 16:05:59 -0500

    Added some additional logging

--- functions/microcontroller.js
+++ functions/microcontroller.js
@@ -3,8 +3,11 @@ const axios = require('axios');
 exports.handler = async function(event) {
   const bodyData = JSON.parse(event.body);
   const {ipAddress, binaryColumnData} = bodyData;
+  const requestURL = `http://${ipAddress}/${binaryColumnData}`;
+  console.log(requestURL);
 
-  axios.get(`http://${ipAddress}/${binaryColumnData}`)
+  axios.get(requestURL)
+    .then((response) => console.log(response))
     .catch((error) => console.log(error));
 
   // fetch(`http://${ipAddress}/${binaryColumnData}`, {
