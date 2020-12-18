ac7cf92557136f4aca8f844ee3ae3a730112456c
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-12-18 12:09:22 -0500

    Update Title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rhythmic Circuit Control`,
+    description: `Control circuits rhythmically with Tone.js.`,
+    author: `@bomanimc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
